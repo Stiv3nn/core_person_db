ea1aaf07cc6a1432bee505a846a5749d38d8cbe2
Stiven Hoxha <stivenhoxha007@gmail.com>
2026-06-29 09:47:58 +0200

    aggiunta logica fetch per recupero e decodifica JSON in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,21 @@ document.addEventListener('DOMContentLoaded', () => {
     // 3. Dichiariamo la funzione asincrona per recuperare i dati
     async function caricaUtenti() {
         try {
-            // Qui dentro metteremo il codice per bussare all'API
+
+            // 5. Qui dentro metteremo il codice per bussare all'API
+            const response = await fetch('/api/utenti');
+
+            // 6. Controllo di sicurezza: la chiamata è andata a buon fine?
+            if (!resposne) {
+                throw new Error(`Errore del server: ${response.status}`);
+            }
+
+            // 7. Estraiamo il JSON e trasformiamolo in un array leggibile
+            const utenti = await response.json();
+
+            // 8. Svuotiamo il testo "Caricamento utenti in corso..."
+            container.innerHTML = '';
+
         } catch (err) {
 
             // Qui dentro gestiamo i problemi se il server è spento
